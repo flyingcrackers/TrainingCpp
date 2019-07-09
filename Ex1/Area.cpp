faa3aebe08df9f697855074f93c22ee15db39e4d
flyingcrackers <52650680+flyingcrackers@users.noreply.github.com>
2019-07-09 18:49:05 +0800

    Create Area.cpp

---
+++ Ex1/Area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float radius, area;
+
+	cout << "Input radius: ";
+
+	cin >> radius;
+
+	area = 3.14159 * radius * radius;
+
+	cout << "Area of circle: " << area << endl;
+
+	system("pause");
+
+}
